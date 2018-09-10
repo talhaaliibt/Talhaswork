d3595a014995773523add6540a280a67b969ab0d
talhaaliibt <43036273+talhaaliibt@users.noreply.github.com>
2018-09-10 10:28:13 -0400

    Add files via upload

---
+++ Assignment 0.py
@@ -0,0 +1,10 @@
+#Talha Ali
+#September 6th 2018
+#ICS4U
+#This program calculates the area of a triangle.
+
+print("Hello this program calculates the area of a triangle")#States the purpose of the program
+height=int(input("What is the height of the triangle in cm?"))#Takes into account the height of the triangle
+base=int(input("What is the length of the base of your triangle in cm?"))#Takes into account the length of the base of the triangle
+area = (base*height)/2 #Computes the formula for the area of a triangle, by multiplying base and height, then dividing the answer by 2.
+print("The area of your triangle is " +str(area) + "cm squared ")#States the area of the triangle
